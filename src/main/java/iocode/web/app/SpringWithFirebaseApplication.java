243e6bd40e33134475203e9ee11f5d307f392bcf
= <=>
2024-06-16 13:35:03 +0100

    Defined Entity for the application

---
+++ src/main/java/iocode/web/app/SpringWithFirebaseApplication.java
@@ -0,0 +1,13 @@
+package iocode.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithFirebaseApplication.class, args);
+	}
+
+}
